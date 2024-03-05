191d9c5212d49d84b42886512be68c6490ad5b3a
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2024-03-04 21:07:06 -0300

    DEL - Remove Search Input

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { FaMagnifyingGlass } from "react-icons/fa6";
 
 import { Button } from "../Button/Button";
 
@@ -34,59 +33,12 @@ const LogoStyled = styled.img`
     }
 `;
 
-const SearchWrapper = styled.div`
-    background-color: var(--search-input-background-color);
-
-    position: relative;
-    
-    justify-self: flex-start;
-
-    border-radius: 20px;
-    margin-right: auto;
-
-    overflow: hidden;
-
-    &:focus-within {
-        outline: 2px solid var(--input-focus-border-color);
-    }
-
-    @media screen and (min-width: 1280px) {
-        width: 17.5rem;
-    }
-`;
-
-const SearchInput = styled.input`
-    background-color: transparent;
-    
-    width: 75%;
-    height: -webkit-fill-available;
-
-    border: none;
-    padding: 0.75rem 1rem;
-
-    outline: none;
-`;
-
-const MagnifyingGlassIcon = styled(FaMagnifyingGlass)`
-    top: 50%;
-    right: 1rem;
-    
-    position: absolute;
-    
-    transform: translateY(-50%);
-`;
-
 export function Header({ user }) {
     return (
         <HeaderStyled>
             <LinkLogoStyled to="/">
                 <LogoStyled src={Logo} alt="Logo do Mundo Geek" />
             </LinkLogoStyled>
-
-            <SearchWrapper>
-                <SearchInput className="tablet desktop" type="text" placeholder="O que deseja buscar?" />
-                <MagnifyingGlassIcon />
-            </SearchWrapper>
 
             {user ? 
                 <img src={User} alt="" />
@@ -100,8 +52,6 @@ export function Header({ user }) {
                     </Button>
                 </Link>
             }
-
-            <FaMagnifyingGlass className="mobile" size="1.25rem" />
         </HeaderStyled>
     );
 }
